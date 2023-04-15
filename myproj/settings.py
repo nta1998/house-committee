e47e2b1d0d55a3f34828f0dad4afa0cbd1ce2c32
nta1998 <111499645+nta1998@users.noreply.github.com>
2023-04-15 15:37:00 +0300

    fix img bug v1.8

--- myproj/settings.py
+++ myproj/settings.py
@@ -180,26 +180,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/images/'
-
-
-# STATICFILES_DIRS = [
-#     f"{BASE_DIR}/static",
-# ]
-
-# MEDIA_ROOT = f"{BASE_DIR}/static/images"
-# STATIC_ROOT = f"{BASE_DIR}/staticfiles"
-
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
-MEDIA_URL = '/media/'
 
-if DEBUG:
-  STATICFILES_DIRS = [f"{BASE_DIR}/static"]
-else:
-  STATIC_ROOT =  f"{BASE_DIR}/static"
-MEDIA_ROOT =  f"{BASE_DIR}/media"
+STATICFILES_DIRS = [
+    f"{BASE_DIR}/static",
+]
+
+MEDIA_ROOT = f"{BASE_DIR}/static/images"
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
