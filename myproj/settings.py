54ce9581a0a5631c1526656752a7012277f0f60b
nta1998 <111499645+nta1998@users.noreply.github.com>
2023-04-15 14:42:46 +0300

    fix img bug v1.5

--- myproj/settings.py
+++ myproj/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/images/'
+MEDIA_URL = '/images/'
 
 
 STATICFILES_DIRS = [
